bdc4faf8c6e50558535c6958a8b6601b8694309c
ilyas <ilyasswadjinny@gmail.com>
2024-03-10 14:58:08 +0100

    fixing setup

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ long_description = "Download Anime/ Movies/ Series with arabic sub from public w
 # m3u8_to_mp4 = "https://github.com/Wadjinny/ArMedia/raw/main/lib/m3u8_To_MP4-0.1.12-py3-none-any.whl"
 # m3u8_to_mp4_path = app_path / "m3u8_To_MP4-0.1.12-py3-none-any.whl"
 # subprocess.run(["wget", m3u8_to_mp4, "-O", str(m3u8_to_mp4_path)], check=True)
-m3u8_path = Path(__file__).parent / "lib/m3u8_To_MP4-0.1.12-py3-none-any.whl"
+# m3u8_path = Path(__file__).parent / "lib/m3u8_To_MP4-0.1.12-py3-none-any.whl"
 # copy m3u8_To_MP4-0.1.12-py3-none-any.whl to app_path
-m3u8_to_mp4_path = app_path / "m3u8_To_MP4-0.1.12-py3-none-any.whl"
-if not m3u8_to_mp4_path.exists():
-    m3u8_to_mp4_path.write_bytes(m3u8_path.read_bytes())
+# m3u8_to_mp4_path = app_path / "m3u8_To_MP4-0.1.12-py3-none-any.whl"
+# if not m3u8_to_mp4_path.exists():
+#     m3u8_to_mp4_path.write_bytes(m3u8_path.read_bytes())
 
 setup(
     name="armedia",
