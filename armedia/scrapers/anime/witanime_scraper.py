4dc8a8d55aefca0fb96126528dbdba650ea1154c
ilyas <ilyasswadjinny@gmail.com>
2024-05-21 19:12:45 +0200

    fixed witanim server links

--- armedia/scrapers/anime/witanime_scraper.py
+++ armedia/scrapers/anime/witanime_scraper.py
@@ -29,43 +29,43 @@ def get_episodes_list(anime_link) -> list[str]:
     episodes = soup.select(".hover.ehover6")
     episodes_info = []
     for episode in episodes:
-        link = re.search(r"openEpisode\('(.*)'\)", episode.select_one(".overlay")["onclick"]).group(1)
+        link = re.search(
+            r"openEpisode\('(.*)'\)", episode.select_one(".overlay")["onclick"]
+        ).group(1)
         link = base64.b64decode(link).decode("utf-8")
         number = episode.select_one("h3 > a").text
         number = re.sub(r"[^\x00-\x7f]", r"", number)
-        
+
         episodes_info.append({"link": link, "number": number})
     return episodes_info
 
 
 def get_all_episodes_server_link(episode_link):
-    #todo: shave headers
-    def get_links_from_yonaplay(yonaplay_link,extension):
+    # todo: shave headers
+    def get_links_from_yonaplay(yonaplay_link, extension):
+        yonaplay_link = yonaplay_link + "&apiKey=7d942435-c790-405c-8381-f682a274b437"
         headers = {
             "referer": f"https://witanime.{extension}/",
         }
 
         payload = {}
-        response = requests.request("GET", yonaplay_link, data=payload,headers=headers)
+        response = requests.request("GET", yonaplay_link, data=payload, headers=headers)
 
         response = response.text
         server_links = re.findall("go_to_player\('(.*?)'\)", response)
         return server_links
 
-    def get_links_from_soraplay(soraplay_link,extension):
-        headers = {
-            "referer": f"https://witanime.{extension}/"
-        }
+    def get_links_from_soraplay(soraplay_link, extension):
+        headers = {"referer": f"https://witanime.{extension}/"}
         response = requests.request("GET", soraplay_link, headers=headers)
         response = response.text
         # go_to_player('//userscloud.com/embed-4z6fyrcnfjns.html')
         server_links = re.findall("go_to_player\('(.*?)'\)", response)
-        for i,link in enumerate(server_links):
+        for i, link in enumerate(server_links):
             if link.startswith("//"):
-                server_links[i] = "https:"+link
+                server_links[i] = "https:" + link
         return server_links
-        
-        
+
     payload = {}
     response = requests.request("GET", episode_link, data=payload)
     response = response.text
@@ -77,34 +77,39 @@ def get_all_episodes_server_link(episode_link):
 
     download_links = soup.select(".quality-list a")
     download_links = [
-        base64.b64decode(a["data-url"]).decode("utf-8") for a in download_links
+        base64.b64decode(a.get("data-url", "")).decode("utf-8") for a in download_links
     ]
     download_links = download_links[::-1]
     server_links.extend(download_links)
     yonaplay_id = list(filter(lambda x: "yonaplay" in x, server_links))
     if yonaplay_id:
         yonaplay_id = yonaplay_id[0]
-        extension = re.search(r"witanime\.(.*?)/",episode_link).group(1)
-        yonaplay_server_links = get_links_from_yonaplay(yonaplay_id,extension)
+        extension = re.search(r"witanime\.(.*?)/", episode_link).group(1)
+        yonaplay_server_links = get_links_from_yonaplay(yonaplay_id, extension)
         server_links.extend(yonaplay_server_links)
     else:
         # print("no yonaplay")
+        debug("no soraplay 'Witanime'")
         pass
-    
+
     # 'https://soraplay.xyz/embed/5QDgM8nSke17f/mirror
-    soraplay_id = list(filter(lambda x: re.search(r"soraplay\.xyz/embed/.*/mirror",x), server_links))
+    soraplay_id = list(
+        filter(lambda x: re.search(r"soraplay\.xyz/embed/.*/mirror", x), server_links)
+    )
     if soraplay_id:
         soraplay_id = soraplay_id[0]
-        extension = re.search(r"witanime\.(.*?)/",episode_link).group(1)
-        soraplay_server_links = get_links_from_soraplay(soraplay_id,extension)
+        extension = re.search(r"witanime\.(.*?)/", episode_link).group(1)
+        soraplay_server_links = get_links_from_soraplay(soraplay_id, extension)
         server_links.extend(soraplay_server_links)
     else:
-        # print("no soraplay")
         pass
-        
 
     return server_links
 
 
 if __name__ == "__main__":
-    print(get_episodes_list("https://witanime.pics/anime/itsudatte-bokura-no-koi-wa-10-cm-datta/"))
+    print(
+        get_episodes_list(
+            "https://witanime.pics/anime/itsudatte-bokura-no-koi-wa-10-cm-datta/"
+        )
+    )
